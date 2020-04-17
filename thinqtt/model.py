898f465190ba00d65d6356783d06564d2b76200b
tinkerborg <tinkerborg@users.noreply.github.com>
2020-04-16 21:54:35 -0400

    reformatting

--- thinqtt/model.py
+++ thinqtt/model.py
@@ -1,14 +1,38 @@
+import uuid
+from thinqtt import ThinQTT
+from urllib import parse
+
 from dataclasses import dataclass, field
+
 
 @dataclass
 class Gateway:
-  country_code: str
-  language_code: str
-  thinq1_uri: str
-  thinq2_uri: str
-  emp_uri: str
-  emp_spx_uri: str
+    country_code: str
+    language_code: str
+    thinq1_uri: str
+    thinq2_uri: str
+    emp_uri: str
+
+    @property
+    def oauth_url(self) -> str:
+        query = parse.urlencode(
+            {
+                "country": self.country_code,
+                "language": self.language_code,
+                "svc_list": ThinQTT.SERVICE_CODE,
+                "client_id": ThinQTT.OAUTH_CLIENT_ID,
+                "division": ThinQTT.DIVISION,
+                "redirect_uri": ThinQTT.OAUTH_REDIRECT_URI,
+                "state": uuid.uuid1().hex,
+                "show_thirdparty_login": ThinQTT.THIRD_PARTY_LOGINS,
+            }
+        )
+        return parse.urljoin(
+            self.emp_uri, "spx/login/signIn?{query}".format(query=query)
+        )
+
 
 @dataclass
 class ClientConfig:
-  gateway: Gateway
+    country_code: str = None
+    language_code: str = None
--- thinqtt/schema.py
+++ thinqtt/schema.py
@@ -2,30 +2,34 @@ import marshmallow_dataclass
 from marshmallow import Schema, fields, post_load, EXCLUDE
 from inflection import camelize
 
+
 class DataSchema(Schema):
-  class Meta:
-    unknown = EXCLUDE
+    class Meta:
+        unknown = EXCLUDE
 
-  def on_bind_field(self, field_name, field):
-    field.data_key = camelize(field_name, uppercase_first_letter=False)
+    @staticmethod
+    def wrap(data_class):
+        return marshmallow_dataclass.class_schema(data_class, base_schema=DataSchema)
 
-  @staticmethod
-  def wrap(data_class):
-    return marshmallow_dataclass.class_schema(data_class, base_schema=DataSchema)
+
+class CamelDataSchema(DataSchema):
+    def on_bind_field(self, field_name, field):
+        field.data_key = camelize(field_name, uppercase_first_letter=False)
+
 
 class ThinQ2Response(Schema):
 
-  result_code = fields.Str(data_key="resultCode")
-  result = fields.Nested(Schema)
+    result_code = fields.Str(data_key="resultCode")
+    result = fields.Nested(Schema)
 
-  def __init__(self, nested=None, **kwargs):
-    self._nested = nested
-    super().__init__(**kwargs)
+    def __init__(self, nested=None, **kwargs):
+        self._nested = nested
+        super().__init__(**kwargs)
 
-  def on_bind_field(self, field_name, field):
-    if field_name is "result" and self._nested != None:
-      field.nested = DataSchema.wrap(self._nested)
+    def on_bind_field(self, field_name, field):
+        if field_name is "result" and self._nested != None:
+            field.nested = CamelDataSchema.wrap(self._nested)
 
-  @post_load
-  def unwrap_result(self, data, **kwargs):
-    return data["result"]
+    @post_load
+    def unwrap_result(self, data, **kwargs):
+        return data["result"]
