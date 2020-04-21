60cb3eeaede4c9f6eee1bede5070d9cf2cccadde
tinkerborg <tinkerborg@users.noreply.github.com>
2020-04-21 18:29:30 -0400

    add dataholder decorator

--- thinqtt/schema.py
+++ thinqtt/schema.py
@@ -4,7 +4,7 @@ import marshmallow_dataclass
 from marshmallow import Schema, fields, pre_load, post_load, EXCLUDE
 from inflection import camelize
 
-from thinqtt.model.auth import OAuthProfile
+from thinqtt.model.auth import UserProfile
 
 
 class DataSchema(Schema):
@@ -23,10 +23,11 @@ class CamelDataSchema(DataSchema):
     def _transform(self, field_name):
         return camelize(field_name, uppercase_first_letter=False)
 
+
 # XXX - make this DRY
 class ProfileResponse(CamelDataSchema):
 
-    account = fields.Nested(marshmallow_dataclass.class_schema(OAuthProfile))
+    account = fields.Nested(marshmallow_dataclass.class_schema(UserProfile))
 
     def _transform(self, field_name):
         return re.sub(r"(?<=[a-z])Id(?=[A-Z]|$)", "ID", super()._transform(field_name))
@@ -52,3 +53,71 @@ class ThinQResponse(Schema):
     @post_load
     def unwrap_result(self, data, **kwargs):
         return data["result"]
+
+
+def dataholder(data_type):
+
+    schema = marshmallow_dataclass.class_schema(data_type, base_schema=Schema)()
+
+    def merge_args(self, kwargs):
+        attrs = {
+            k: getattr(self, k, None) for k in schema.fields.keys() if not k in kwargs
+        }
+        defaults = schema.dump({k: v for k, v in attrs.items() if v is not None})
+        return {**defaults, **kwargs}
+
+    class AttrDict(dict):
+        def __getattr__(self, attr):
+            return self.get(attr, None)
+
+    class DataHolder(object):
+        _data: data_type = None
+
+        def __init__(self, data=None, **kwargs):
+            if data is None:
+                self._data = AttrDict(kwargs)
+                self._data = schema.load(merge_args(self, kwargs))
+            else:
+                self._data = data
+
+        @classmethod
+        def load(cls, data):
+            return cls(schema.load(data))
+
+        @property
+        def __dict__(self):
+            return schema.dump(self._data)
+
+        def __getattr__(self, attr):
+            if hasattr(self._data, attr):
+                return getattr(self._data, attr)
+            return super().__getattr__(self, attr)
+
+        def __setattr__(self, attr, value):
+            if isinstance(self._data, data_type) and hasattr(self._data, attr):
+                setattr(self._data, attr, value)
+            else:
+                super().__setattr__(attr, value)
+
+    def class_wrapper(base_class):
+        return type(base_class.__name__, (DataHolder, base_class), {})
+
+    return class_wrapper
+
+
+def initializer(obj, attr="_data"):
+    @property
+    def inner(self):
+        data = getattr(self, attr)
+        if data is None:
+            return obj(self)
+
+        value = getattr(data, obj.__name__)
+        if value is not None:
+            return value
+
+        value = obj(self)
+        setattr(data, obj.__name__, value)
+        return value
+
+    return inner
