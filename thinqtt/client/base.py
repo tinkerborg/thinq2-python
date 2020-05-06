651f4d729ab6d01bdf91b8db3bbc87e4a0f2b5f8
tinkerborg <tinkerborg@users.noreply.github.com>
2020-05-06 03:12:11 -0400

    revert headers removal

--- thinqtt/client/base.py
+++ thinqtt/client/base.py
@@ -4,5 +4,6 @@ from uplink import Consumer
 
 
 class BaseClient(Consumer):
-    def __init__(self, base_url=None, **kwargs):
+    def __init__(self, base_url=None, headers={}, **kwargs):
         super().__init__(path.join(base_url or self.base_url, ""), **kwargs)
+        self.session.headers.update(headers)
